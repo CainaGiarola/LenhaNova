836f598727cd4834e22bc8bb21d06619ad791ee7
ynextson <caina447@gmail.com>
2024-03-23 15:42:12 -0300

    alteraded identation for scripts

--- public/js/sbImage.script.js
+++ public/js/sbImage.script.js
@@ -7,10 +7,10 @@ const firebaseConfig = {
     messagingSenderId: "99692343858",
     appId: "1:99692343858:web:271d58a1b6a19d8686022b",
     measurementId: "G-19YG6E8RVG"
-  };
+};
 
 firebase.initializeApp(firebaseConfig);
-  
+
 var fileText = document.querySelector(".fileText");
 var uploadPercentage = document.querySelector(".uploadPercentage");
 var progress = document.querySelector(".progress");
@@ -18,32 +18,32 @@ var percentVal;
 var fileItem;
 var fileName;
 var img = document.querySelector(".img");
- function getFile(e) {
+function getFile(e) {
     fileItem = e.target.files[0];
     fileName = fileItem.name;
     fileText.innerHTML = fileName;
 }
 
-function uploadImage(){
-    let storageRef = firebase.storage().ref("images/"+filename);
+function uploadImage() {
+    let storageRef = firebase.storage().ref("images/" + filename);
     let uploadTask = storageRef.put(fileName);
 
-    uploadTask.on("state_changed", (snapshot) =>{
+    uploadTask.on("state_changed", (snapshot) => {
         console.log(snapshot);
-        percentVal = Math.floor((snapshot.bytesTransferred/snapshot.totalBytes)*100);
+        percentVal = Math.floor((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
         console.log(percentVal);
-        uploadPercentage.innerHTML = percentVal+"%";
-        progress.style.width=percentVal+"%";
+        uploadPercentage.innerHTML = percentVal + "%";
+        progress.style.width = percentVal + "%";
 
-    },(error)=>{
+    }, (error) => {
         console.log("Error is ", error);
-    },()=>{
-        uploadTask.snapshot.ref.getDownloadURL().then((url)=>{
+    }, () => {
+        uploadTask.snapshot.ref.getDownloadURL().then((url) => {
             console.log("URL", url);
 
-            if(url != ""){
+            if (url != "") {
                 img.setAttribute("src", url);
-                img.style.display="block";
+                img.style.display = "block";
             }
         })
     })
