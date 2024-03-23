eb4db61b80be26baf38dd28d25319570edb75b12
ynextson <caina447@gmail.com>
2024-03-23 19:00:49 -0300

    alterações do codigo

--- public/js/sbImage.script.js
+++ public/js/sbImage.script.js
@@ -1,5 +1,5 @@
-
 import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAYpQUk1SDs4dxdsW_HIwybiM0thFdwpaM",
@@ -29,27 +29,53 @@ function getFile(e) {
     fileText.innerHTML = fileName;
 }
 
-function uploadImage() {
-    let storageRef = firebase.storage().ref("images/" + filename);
-    let uploadTask = storageRef.put(fileName);
+document.getElementById('fileImp').addEventListener('change', getFile);
 
-    uploadTask.on("state_changed", (snapshot) => {
+function uploadImage() {
+    const storage = getStorage(firebase);
+    let storageRef = ref(storage, "images/" + fileName);
+    let uploadTask = uploadBytesResumable(storageRef, fileItem);
+
+    uploadTask.on('state_changed', (snapshot) => {
+        // Seu código para lidar com o progresso
         console.log(snapshot);
         percentVal = Math.floor((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
         console.log(percentVal);
         uploadPercentage.innerHTML = percentVal + "%";
         progress.style.width = percentVal + "%";
-
     }, (error) => {
+        // Seu código para lidar com erro
         console.log("Error is ", error);
     }, () => {
-        uploadTask.snapshot.ref.getDownloadURL().then((url) => {
-            console.log("URL", url);
-
-            if (url != "") {
-                img.setAttribute("src", url);
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+            // Seu código para lidar com a URL de download
+            if (downloadURL != "") {
+                img.setAttribute("src", downloadURL);
                 img.style.display = "block";
             }
-        })
-    })
+        });
+    });
 }
+
+// function uploadImage() {
+//     let storageRef = firebase.storage().ref("images/" + filename);
+//     let uploadTask = storageRef.put(fileName);
+
+//     uploadTask.on("state_changed", (snapshot) => {
+//         console.log(snapshot);
+//         percentVal = Math.floor((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+//         console.log(percentVal);
+//         uploadPercentage.innerHTML = percentVal + "%";
+//         progress.style.width = percentVal + "%";
+
+//     }, (error) => {
+//         console.log("Error is ", error);
+//     }, () => {
+//         uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+//             console.log("URL", url);
+
+            
+//         })
+//     })
+// }
