aab12d61faf4e2a1ae45947c7532014b5d470613
ynextson <caina447@gmail.com>
2024-03-25 20:43:08 -0300

    Alterado versão firebaseJS

--- public/js/sbImage.script.js
+++ public/js/sbImage.script.js
@@ -1,8 +1,8 @@
 // import { initializeApp } from "firebase/app";
 // import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+// import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAYpQUk1SDs4dxdsW_HIwybiM0thFdwpaM",
